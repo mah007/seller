519abf6647fbefc97c42ce9b2ba26285c8a7180b
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-06-27 19:54:45 +0700

    update python version to 3.6.1

--- server/setup.sh
+++ server/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Install psycopg2"
-pip install psycopg2
+pip3 install psycopg2
 
 echo "Install Flask"
-pip install flask
+pip3 install flask
 
 echo "Install flask_cors"
-pip install -U flask-cors
+pip3 install -U flask-cors
+
+echo "Install flask_cors"
+pip3 install requests
