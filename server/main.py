9083400975eb95477a6797c78bb4577a8a195975
Asus\Asus N550 <hoangbeatsb3@gmail.com>
2017-08-07 20:58:55 +0700

    Reverse config

--- server/main.py
+++ server/main.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
   # userManager.initialize()
   # orderManager.initialize()
 
-  app.run(debug=True, threaded=True)
-  # app.run(host='0.0.0.0', debug=True, port=5000, threaded=True)
+  # app.run(debug=True, threaded=True)
+  app.run(host='0.0.0.0', debug=True, port=5000, threaded=True)
 
 
 
