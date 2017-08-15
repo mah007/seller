c3b010b7deec37b14dd6c94f4278e41cdd1467da
Asus\Asus N550 <hoangbeatsb3@gmail.com>
2017-08-15 21:32:58 +0700

    Auto Login To Lazada Page

--- server/setup.sh
+++ server/setup.sh
@@ -24,6 +24,9 @@ pip3 install pyjwt
 echo "Install bycrypt"
 pip3 install bcrypt
 
+echo "Install selenium"
+pip3 install selenium
+
 # For bycrypt dependencies
 # For Debian and Ubuntu, the following command will ensure that the required dependencies are installed:
 # $ sudo apt-get install build-essential libffi-dev python-dev
