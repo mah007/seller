c58abfdb48ca8e2d63152fa0c66d4bb52118543a
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-09-09 01:44:58 +0700

    updated OrderDao.

--- server/database/order_dao.py
+++ server/database/order_dao.py
@@ -10,9 +10,15 @@ class OrderDao(object):
                 id              INT AUTO_INCREMENT primary key NOT NULL,
                 order_id        VARCHAR(50)     NOT NULL,
                 order_number    VARCHAR(50)     NOT NULL,
+                price           VARCHAR(20)     NOT NULL,   # 113,000.00
+                customer_name   VARCHAR(100)    NOT NULL,
+                voucher_code    VARCHAR(50)     NOT NULL,
+                voucher_price   VARCHAR(50)     NOT NULL,
+                status          VARCHAR(100)    NOT NULL,
+                created_at      VARCHAR(30)     NOT NULL,   # 2017-09-08 01:12:04
+                updated_at      VARCHAR(30)     NOT NULL,   # 2017-09-08 01:12:04
                 order_json      TEXT            NOT NULL,
                 user_id         VARCHAR(30)     NOT NULL,
-                created_at      VARCHAR(30)     NOT NULL
                 );'''
         DatabaseHelper.execute(query)
 
@@ -20,31 +26,50 @@ class OrderDao(object):
     # Insert order
     # --------------------------------------------------------------------------
     def insert(self, user, order):
+        query = '''INSERT INTO order_management(
+                            order_id,
+                            order_number,
+                            price,
+                            customer_name,
+                            voucher_code,
+                            voucher_price,
+                            status,
+                            created_at,
+                            updated_at,
+                            order_json,
+                            user_id)
+                    VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')'''.format(
+                    order['order_id'], order['order_number'], order['price'], order['customer_name'], order['voucher_code'],
+                    order['voucher_price'], order['status'], order['created_at'], order['updated_at'], order['order_json'], user['id'])
         try:
-            query = '''INSERT INTO order_management(order_id, order_number, order_json, user_id, created_at) VALUES ('{}', '{}', '{}', {}, {})'''.format(
-                    order['order_id'], order['order_number'], order['order_json'], user['id'], order['created_at'])
             DatabaseHelper.execute(query)
             return ExceptionUtils.success()
         except Exception as ex:
-            return ExceptionUtils.error('''User: {}-{}, Insert Order: {} failed: {}'''.format(user['id'], user['username'], order['order_number'], str(ex)))
+            return ExceptionUtils.error('''User: {}-{}, Insert-Order exception: {}'''.format(user['username'], user['id'], str(ex)))
 
     # --------------------------------------------------------------------------
-    # delete all order
+    # Check Order exsits
     # --------------------------------------------------------------------------
-    def deleteAllOrders(self, user):
+    def isOrderExist(self, user, order):
+        query = '''SELECT * FROM order_management WHERE order_id = '{}' AND user_id = '{}'' '''.format(order['order_id'], user['id'])
         try:
-            query = '''DELETE FROM order_management WHERE user_id = {} '''.format(user['id'])
-            DatabaseHelper.execute(query)
-            return ExceptionUtils.success()
+            conn = DatabaseHelper.getConnection()
+            cur = conn.cursor()
+            cur.execute(query)
+            rows = cur.fetchall()
+            result = False if not rows else True;
+            conn.close()
+            return result
         except Exception as ex:
-            return ExceptionUtils.error('''User: {}-{}, Delete all orders is error: {}'''.format(user['id'], user['username'], str(ex)))
+            print('''User: {}-{}, Check-Exist-Order exception: {}'''.format(user['username'], user['id'], str(ex)))
+            return False
 
     # --------------------------------------------------------------------------
     # Get order by order number
     # --------------------------------------------------------------------------
     def getOrderByOrderNumber(self, user, orderNumber):
+        query = '''SELECT * from order_management WHERE user_id = '{}' AND order_number = '{}' '''.format(user['id'], orderNumber)
         try:
-            query = '''SELECT * from order_management WHERE user_id = '{}' AND order_number = '{}' '''.format(user['id'], orderNumber)
             conn = DatabaseHelper.getConnection()
             cur = conn.cursor()
             cur.execute(query)
@@ -52,22 +77,42 @@ class OrderDao(object):
             rows = cur.fetchall()
             if not rows:
                 conn.close()
-                return ExceptionUtils.error('''Order number: {} is not found'''.format(orderNumber))
+                return ExceptionUtils.error('''User: {}-{}, Get Order: {} is not found'''.format(user['username'], user['id'], orderNumber))
 
             order = {}
             for row in rows:
                 order['id'] = row[0]
                 order['order_id'] = row[1]
                 order['order_number'] = row[2]
-                order['order_json'] = row[3]
-                order['user_id'] = row[4]
-                order['created_at'] = row[5]
+                order['price'] = row[3]
+                order['customer_name'] = row[4]
+                order['voucher_code'] = row[5]
+                order['voucher_price'] = row[6]
+                order['status'] = row[7]
+                order['created_at'] = row[8]
+                order['updated_at'] = row[9]
+                order['order_json'] = row[10]
+                order['user_id'] = row[11]
 
             conn.close()
             return order
         except Exception as ex:
-            return ExceptionUtils.error('''User: {}-{}, Get Order: {} failed: {}'''.format(user['id'], user['username'], orderNumber, str(ex)))
+            return ExceptionUtils.error('''User: {}-{}, Order-Number: {}, Get-Order-By-Order-Number exception {}'''.format(user['username'], user['id'], orderNumber, str(ex)))
 
+    # --------------------------------------------------------------------------
+    # Update Order State
+    # --------------------------------------------------------------------------
+    def updateOrderState(self, user, order):
+        query = '''UPDATE order_management set
+                        status = '{}',
+                        updated_at = '{}',
+                        order_json = {}
+                    WHERE id = '{}' '''.format(order['status'], order['updated_at'], order['order_json'], order['id'])
+        try:
+            DatabaseHelper.execute(query)
+            return ExceptionUtils.success()
+        except Exception as ex:
+            return ExceptionUtils.error('''User: {}-{}, Order-Number: {}, Update-Order-State exception: {}'''.format(user['username'], user['id'], orderNumber, str(ex)))
 
 
 
