68cb31fa340b26e309162579ccecf5c1a4d931ab
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-09-09 11:13:02 +0700

    Added main.py for cronjob.

---
+++ server/cronjob/main.py
@@ -0,0 +1,31 @@
+import time
+import threading
+from database.user_dao import UserDao
+from cronjob.auto_price_worker import AutoPriceWorker
+from config import RunnerConfig
+
+
+if __name__ == "__main__":
+
+  starttime=time.time()
+  userDao = UserDao();
+
+  while True:
+    users = userDao.getAll()
+    if (users):
+      for user in users:
+        clone = AutoPriceWorker({"user": user})
+        try:
+          clone.daemon = True
+          clone.start()
+        except Exception as ex:
+          clone.join(0)
+          print ("Error: unable to start thread: ", ex)
+
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+
+
+
+
+
