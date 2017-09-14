ff088a6000f33712cd817e69b7458a32320a4f2a
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-09-14 20:32:32 +0700

    Updated setup.

--- server/setup.sh
+++ server/setup.sh
@@ -1,51 +1,40 @@
 #!/bin/bash
 
-echo "Install chromedriver"
-brew install chromedriver
-
-echo "Install Pipreqs"
-pip3 install pipreqs
-
-echo "Generate dependency requirements.txt"
-pipreqs --force .
-
-echo "Install dependencies"
-pip3 install -v -r requirements.txt
-
-
-
-# echo "Install psycopg2"
+# echo "Install PostgreSQL"
 # pip3 install psycopg2
 
-# echo "Install Flask"
-# pip3 install flask
+echo "Install Flask"
+pip3 install flask
 
-# echo "Install flask cross platform"
-# pip3 install -U flask-cors
+echo "Install flask cross platform"
+pip3 install -U flask-cors
 
-# echo "Install requests"
-# pip3 install requests
+echo "Install requests"
+pip3 install requests
 
-# echo "Install json"
-# pip3 install simplejson
+echo "Install json"
+pip3 install simplejson
 
-# echo "Install lxml"
-# pip3 install lxml
+echo "Install lxml"
+pip3 install lxml
 
-# echo "Install jwt"
-# pip3 install pyjwt
+echo "Install jwt"
+pip3 install pyjwt
 
-# echo "Install mysql"
-# pip3 install pymysql
+echo "Install mysql"
+pip3 install pymysql
 
-# echo "Install bycrypt"
-# pip3 install bcrypt
+echo "Install bycrypt"
+pip3 install bcrypt==3.1.0
+
+echo "Install pytz"
+pip3 install pytz
 
 # For bycrypt dependencies
 # For Debian and Ubuntu, the following command will ensure that the required dependencies are installed:
-# $ sudo apt-get install build-essential libffi-dev python-dev
+# sudo apt-get install build-essential libffi-dev python-dev
 # For Fedora and RHEL-derivatives, the following command will ensure that the required dependencies are installed:
-# $ sudo yum install gcc libffi-devel python-devel
+# sudo yum install gcc libffi-devel python-devel
 
 
 
