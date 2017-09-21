5545708c12ea575351c60fd3a5a2c8f343a77a7f
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-09-21 23:55:44 +0700

    Added get product by created after.

--- server/lazada_api/lazada_product_api.py
+++ server/lazada_api/lazada_product_api.py
@@ -11,7 +11,7 @@ class LazadaProductApi(object):
 	#-----------------------------------------------------------------------------
 	# Get Produts from Lazada
 	#-----------------------------------------------------------------------------
-	def getProducts(self, user, constant):
+	def getProducts(self, user, timestamp):
 		parameters = {
 		'Action': 'GetProducts',
 		'Format':'JSON',
@@ -50,5 +50,47 @@ class LazadaProductApi(object):
 			return ExceptionUtils.error('''Get Products is error: {}'''.format(resp.status_code))
 		except Exception as ex:
 			return ExceptionUtils.error('''Get Products is error: {}'''.format(str(ex)))
+
+	#-----------------------------------------------------------------------------
+	# Get Produts by CreatedAfter
+	#-----------------------------------------------------------------------------
+	def getProductsWithCreatedAfter(self, user, constant):
+		parameters = {
+		'Action': 'GetProducts',
+		'Format':'JSON',
+		'Timestamp': LazadaApiHelper.getCurrentUTCTime(),
+		'UserID': user['lazada_user_id'],
+		'Version': '1.0',
+		'CreatedAfter': LazadaApiHelper.getCurrentUTCTime(),
+		'Filter': 'all',
+		'Limit': LazadaAPI.LIMIT
+		}
+
+		parameters['Signature'] = LazadaApiHelper.generateSignature(parameters, user['lazada_api_key'])
+		url = "{}/?Action={}&Format={}&Timestamp={}&UserID={}&Version={}&Signature={}&CreatedBefore={}&Filter={}&Offset={}&Limit={}".format(
+						LazadaAPI.ENDPOINT,
+		 				parameters["Action"],
+		 				parameters["Format"],
+		 				LazadaApiHelper.formatTimestamp(parameters["Timestamp"]),
+		 				parameters["UserID"],
+		 				parameters["Version"],
+		 				parameters["Signature"],
+		 				LazadaApiHelper.formatTimestamp(parameters["CreatedAfter"]),
+		 				parameters["Filter"],
+		 				parameters["Limit"])
+		try:
+			resp = requests.get(url)
+			if resp.status_code == 200:
+				response = json.loads(resp.text)
+				if ('ErrorResponse' in response):
+					return ExceptionUtils.returnError('''Get Products is error: ''', response)
+
+				data = response['SuccessResponse']['Body']
+				return data['Products'], data['TotalProducts']
+
+			return ExceptionUtils.error('''Get Products is error: {}'''.format(resp.status_code))
+		except Exception as ex:
+			return ExceptionUtils.error('''Get Products is error: {}'''.format(str(ex)))
+
 
 
