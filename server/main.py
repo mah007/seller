5e2df998676c5f40542144f314433f65d50befa5
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-09-29 00:35:54 +0700

    Updated main.py to fit server environment.

--- server/main.py
+++ server/main.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
   priceByTimeManager.initialize()
   productManager.initialize()
 
-  app.run(debug=True, threaded=True)
-  # app.run(host='0.0.0.0', debug=True, port=5000, threaded=True)
+  # app.run(debug=True, threaded=True)
+  app.run(host='0.0.0.0', debug=True, port=5000, threaded=True)
 
 
 
